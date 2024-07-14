8c063b8ebb89e1c43a56e9b93e349e3d88c58d08
joseneisantana <joseneisantana@yahoo.com>
2024-07-14 11:38:13 -0300

    Finalizada calculadora

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+    <script setup>
+    import Calculadora from './components/Calculadora.vue';
+    </script>
+<template>
+  <div id="app">
+    <h1>Calculadora Aritmética</h1>
+    <Calculadora />
+  </div>
+</template>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #1c79d6;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Calculadora.vue
@@ -0,0 +1,54 @@
+  <script setup>
+  import { ref, computed } from 'vue';
+  
+  const num1 = ref(0);
+  const num2 = ref(0);
+  const operacao = ref('+');
+  
+  const resultado = computed(() => {
+    switch (operacao.value) {
+      case '+':
+        return num1.value + num2.value;
+      case '-':
+        return num1.value - num2.value;
+      case '*':
+        return num1.value * num2.value;
+      case '/':
+        return num2.value !== 0 ? num1.value / num2.value : 'Erro: Divisão por zero';
+      default:
+        return 0;
+    }
+  });
+  </script>
+<template>
+    <div class="calculadora">
+      <div class="input-group mb-3">
+        <input type="number" class="form-control" v-model.number="num1" placeholder="Número 1">
+        <select class="form-select" v-model="operacao">
+          <option value="+">+</option>
+          <option value="-">-</option>
+          <option value="*">*</option>
+          <option value="/">/</option>
+        </select>
+        <input type="number" class="form-control" v-model.number="num2" placeholder="Número 2">
+      </div>
+      <h3>Resultado: {{ resultado }}</h3>
+    </div>
+  </template>
+  
+  
+  <style scoped>
+  .calculadora {
+    max-width: 400px;
+    margin: auto;
+    padding: 20px;
+    border: 3px solid #184098;
+    border-radius: 10px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    background-color: aqua;
+  }
+  .input-group {
+    display: flex;
+  }
+  </style>
+  
